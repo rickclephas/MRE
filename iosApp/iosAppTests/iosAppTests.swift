3473d6d64f818fe3a482182b3d38d12ee0d2d4d8
Rick Clephas <rclephas@gmail.com>
2024-02-02 19:28:13 +0100

    Add tests to iosApp

---
+++ iosApp/iosAppTests/iosAppTests.swift
@@ -0,0 +1,27 @@
+//
+//  iosAppTests.swift
+//  iosAppTests
+//
+//  Created by Rick Clephas on 02/02/2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import XCTest
+import shared
+
+final class iosAppTests: XCTestCase {
+
+    func testSwitchPropertyToFalseA() throws {
+        let a = ImplA()
+        XCTAssertTrue(a.property.boolValue)
+        a.property = false
+        XCTAssertFalse(a.property.boolValue)
+    }
+    
+    func testSwitchPropertyToFalseB() throws {
+        let b = ImplB()
+        XCTAssertTrue(b.property.boolValue)
+        b.property = false
+        XCTAssertFalse(b.property.boolValue)
+    }
+}
